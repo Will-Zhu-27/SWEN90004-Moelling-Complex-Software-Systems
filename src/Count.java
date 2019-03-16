4d52f81db81b8f2c2aa7d713f593bd644d7cd561
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2019-03-17 01:13:05 +1100

    Update Count.java

---
+++ src/Count.java
@@ -0,0 +1,25 @@
+class Count extends Thread {
+	static volatile int n = 0;
+	
+	public void run() {
+		int temp;
+		for (int i = 0; i < 10; i++) {
+			temp = n;
+			n = temp + 1;
+		}
+	}
+	
+	public static void main (String[] args) {
+		Count p = new Count();
+		Count q = new Count();
+		p.start();
+		q.start();
+		try {
+			p.join();
+			q.join();
+		} catch (InterruptedException e) {
+			
+		}
+		System.out.println(" The final value of n is " + n);
+	}
+}
