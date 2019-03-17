94a8bf657b3e7419dc4a7d7a83b682e89a342ade
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2019-03-17 15:47:56 +1100

    Create Attempt1.java

---
+++ src/Workshop2/Attempt1.java
@@ -0,0 +1,37 @@
+class Count extends Thread {
+
+    // number of increments per thread
+    static int N = 1000;
+
+    // shared data
+    static volatile int counter = 0;
+    static int turn = 0;
+    
+    // protocol variables
+    // ...
+
+    public void run() {
+      int temp;
+      for (int i = 0; i < N; i++) {
+        // non-critical section
+
+        // pre-protocol section
+
+        // critical section
+        temp = counter;
+        counter = temp + 1;
+
+        // post-protocol section
+      }
+    }
+
+    public static void main(String[] args) {
+      Count p = new Count();
+      Count q = new Count();
+      p.start();
+      q.start();
+      try { p.join(); q.join(); }
+      catch (InterruptedException e) { }
+      System.out.println("The final value of the counter is " + counter);
+    }
+}
